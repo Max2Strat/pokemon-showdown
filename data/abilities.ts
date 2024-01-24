f0badb8f521433e1b0a0d39f42cdf9544e4bb8a8
Max2Strat <156451136+Max2Strat@users.noreply.github.com>
2024-01-24 12:09:25 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -65,6 +65,25 @@ fog: {
 		rating: 4,
 		num: 320,
 	},
+desert: {
+		onStart(source) {
+			this.field.setWeather('sandstorm');
+		},
+
+   	onBasePowerPriority: 21,
+		onBasePower(basePower, attacker, defender, move) {
+			if (this.field.isWeather('sandstorm')) {
+				if (move.type === 'Rock' || move.type === 'Ground' || move.type === 'Steel') {
+					this.debug('Desert boost');
+					return this.chainModify([5325, 4096]);
+				}
+			}
+		},
+		flags: {},
+		name: "Desert",
+		rating: 4,
+		num: 45,
+	},
 	solublebody: {
         onSourceModifyDamage(damage, source, target, move) {
             let mod = 1;
