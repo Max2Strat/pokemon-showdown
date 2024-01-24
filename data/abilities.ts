996ec8d3e8fa4e3153b46784bb550b9891fe0ed6
Max2Strat <156451136+Max2Strat@users.noreply.github.com>
2024-01-24 23:18:56 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -87,7 +87,7 @@ desert: {
 	solublebody: {
         onSourceModifyDamage(damage, source, target, move) {
             let mod = 1;
-            if (move.type === 'Fire') mod *= 2;
+            if (move.type === 'Electric') mod *= 2;
             if (move.flags['contact']) mod /= 2;
             return this.chainModify(mod);
         },
