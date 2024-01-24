c417302972b6f26addb93626aeec0d71decaa7a0
Max2Strat <156451136+Max2Strat@users.noreply.github.com>
2024-01-24 19:14:57 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -238,6 +238,33 @@ desert: {
 		name: "Dimension of the Braves",
 		rating: 4,
 		num: 324,
+},
+	temporallaw: {
+	onDamage(damage, target, source, effect) {
+			if (
+				effect.effectType === "Move" &&
+				!effect.multihit &&
+				(!effect.negateSecondary && !(effect.hasSheerForce && source.hasAbility('sheerforce')))
+			) {
+				this.effectState.checkedTemporalLaw = false;
+			} else {
+				this.effectState.checkedTemporalLaw = true;
+			}
+		},	
+	onAfterMoveSecondary(target, source, move) {
+			this.effectState.checkedDimensionoftheBraves = true;
+			if (!source || source === target || !target.hp || !move.totalDamage) return;
+			const lastAttackedBy = target.getLastAttackedBy();
+			if (!lastAttackedBy) return;
+			const damage = move.multihit ? move.totalDamage : lastAttackedBy.damage;
+			if (target.hp <= target.maxhp / 2 && target.hp + damage > target.maxhp / 2) {
+				pokemon.heal(pokemon.baseMaxhp / 2);
+			}
+		},
+		flags: {},
+		name: "Temporal Law",
+		rating: 4.5,
+		num: 144,
 	},
    dualstrikes: {
         onPrepareHit(source, target, move) {
