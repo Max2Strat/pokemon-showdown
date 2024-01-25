57f97e50499a987bf2f6d9d378875c61664e50b2
Max2Strat <156451136+Max2Strat@users.noreply.github.com>
2024-01-26 00:38:46 +0100

    Update abilities.ts

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -50,6 +50,8 @@ export const AbilitiesText: {[k: string]: AbilityText} = {
 	huntersfang: {
 		name: "Hunter's Fang",
 		desc: "This Pokemon's bite-based attacks have their power multiplied by 1.5. 20% chance to poison the target.",
+		shortDesc: "Bite-based attacks are x1.5 and have 20% chance to poison the target.",
+
 	},
 	aerilate: {
 		name: "Aerilate",
